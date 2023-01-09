108ce072a90e510155b4ebd6e203fee4632a7920
XU XILIE <xuxilie@cgpa3.comp.nus.edu.sg>
2023-01-09 16:41:22 +0800

    update_ACL_imagenet

---
+++ ACL_ImageNet/ACL_imagnet.sh
@@ -0,0 +1,21 @@
+### Pre-training ###
+nohup python ACL.py results/Standard_full --gpu 0 --method full &
+
+nohup python ACL.py results/ACL_random_005 --ACL_DS --gpu 0 --method random --fraction 0.05 &
+
+nohup python ACL.py results/ACL_KL_005 --ACL_DS --gpu 0 --method coreset--fraction 0.05 &
+
+### Finetuning ###
+PT=results/ACL_KL_005/model.pt
+NAME=ACL_KL_005
+
+nohup python adv_tune.py --gpu 0 --out_dir cifar10_robust_fully_finetuning/${NAME} --dataset cifar10 --lr 0.1 --resume $PT &
+nohup python adv_tune.py --gpu 0 --out_dir cifar100_robust_fully_finetuning/${NAME} --dataset cifar100 --lr 0.1 --resume $PT &
+nohup python adv_tune.py --gpu 0 --out_dir cifar10_robust_partilly_finetuning/${NAME} --linear --dataset cifar10 --lr 0.01 --resume $PT &
+nohup python adv_tune.py --gpu 0 --out_dir cifar100_robust_partilly_finetuning/${NAME} --linear --dataset cifar100 --lr 0.01 --resume $PT &
+
+nohup python transfer.py --gpu 0 --out_dir cifar10_standard_fully_finetuning/${NAME} --dataset cifar10 --lr 0.1 --resume $PT &
+nohup python transfer.py --gpu 0 --out_dir cifar100_standard_fully_finetuning/${NAME} --dataset cifar100 --lr 0.1 --resume $PT &
+nohup python transfer.py --gpu 0 --out_dir cifar10_standard_partilly_finetuning/${NAME} --linear --dataset cifar10 --lr 0.01 --resume $PT &
+nohup python transfer.py --gpu 0 --out_dir cifar100_standard_partilly_finetuning/${NAME} --linear --dataset cifar100 --lr 0.01 --resume $PT &
+
