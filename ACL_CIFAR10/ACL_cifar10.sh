330da1e764f47bfe3056ca007699501a445484a1
XU XILIE <xuxilie@cgpa5.comp.nus.edu.sg>
2023-01-17 23:10:04 +0800

    update_sh

--- ACL_CIFAR10/ACL_cifar10.sh
+++ ACL_CIFAR10/ACL_cifar10.sh
@@ -14,15 +14,15 @@ nohup python ACL.py results/RCS_KL_02 --gpu 0 --ACL_DS --dataset cifar10 --metho
 ### Finetuning ###
 MODEL_PATH=results/adversarial_full/model.pt
 
-nohup python finetune.py cifar100_robust_fully_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f1 &
-nohup python finetune.py svhn_robust_fully_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f1 &
+nohup python finetune.py cifar100_robust_full_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f1 &
+nohup python finetune.py svhn_robust_full_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f1 &
 
-nohup python finetune.py cifar100_robust_partially_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 &
-nohup python finetune.py svhn_robust_partially_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 &
+nohup python finetune.py cifar100_robust_partial_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 &
+nohup python finetune.py svhn_robust_partial_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 &
 
-nohup python finetune.py cifar100_standard_fully_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
-nohup python finetune.py svhn_standard_fully_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
+nohup python finetune.py cifar100_standard_full_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
+nohup python finetune.py svhn_standard_full_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
 
-nohup python finetune.py cifar100_standard_partially_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
-nohup python finetune.py svhn_standard_partially_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
+nohup python finetune.py cifar100_standard_partial_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset cifar100 --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
+nohup python finetune.py svhn_standard_partial_finetune/adversarial_full --gpu 0 --checkpoint $MODEL_PATH --dataset svhn --cvt_state_dict --bnNameCnt 1 --decreasing_lr 40,60 --epochs 100 --fixmode f3 --trainmode normal &
 
--- SAT_ImageNet_224/SAT_imagenet_224.sh
+++ SAT_ImageNet_224/SAT_imagenet_224.sh
@@ -12,8 +12,8 @@ nohup python SAT.py --gpu 0,1,2,3 --net r50 --out_dir KL_02 --method RCS --fract
 NAME = AT_KL_005
 PT = KL_005/checkpoint.pth.tar
 
-nohup python transfer.py --gpu 0 --dataset cifar10  --out_dir cifar10_standard_fully_finetuning/${NAME} --net r50 --lr 0.001 --resume $PT  &
-nohup python transfer.py --gpu 0 --dataset cifar100  --out_dir cifar100_standard_fully_finetuning/${NAME} --net r50 --lr 0.001 --resume $PT  &
+nohup python transfer.py --gpu 0 --dataset cifar10  --out_dir cifar10_standard_full_finetuning/${NAME} --net r50 --lr 0.001 --resume $PT  &
+nohup python transfer.py --gpu 0 --dataset cifar100  --out_dir cifar100_standard_full_finetuning/${NAME} --net r50 --lr 0.001 --resume $PT  &
 
-nohup python transfer.py --gpu 0 --dataset cifar10  --out_dir cifar10_standard_partially_finetuning/${NAME} --net r50 --lr 0.01 --resume $PT  &
-nohup python transfer.py --gpu 0 --dataset cifar100  --out_dir cifar100_standard_partially_finetuning/${NAME} --net r50 --lr 0.01 --resume $PT  &
+nohup python transfer.py --gpu 0 --dataset cifar10  --out_dir cifar10_standard_partial_finetuning/${NAME} --net r50 --lr 0.01 --resume $PT  &
+nohup python transfer.py --gpu 0 --dataset cifar100  --out_dir cifar100_standard_partial_finetuning/${NAME} --net r50 --lr 0.01 --resume $PT  &
